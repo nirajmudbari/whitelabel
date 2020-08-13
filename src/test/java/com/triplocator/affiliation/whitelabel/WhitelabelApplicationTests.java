1dc072ad75053daad97d52b7c4a0dad682e50cc5
Niraj Mudbari <niraaz2010@gmail.com>
2020-08-13 10:09:17 +0545

    starting

---
+++ src/test/java/com/triplocator/affiliation/whitelabel/WhitelabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triplocator.affiliation.whitelabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitelabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
