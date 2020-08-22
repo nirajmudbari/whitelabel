cda1941336a4c6d0064b4889d9b46bd124df57dd
anilmaharjan <anilmaharjan87@gmail.com>
2020-08-22 15:38:50 +0545

    tour page

--- app.js
+++ app.js
@@ -69,8 +69,84 @@ $(document).ready(function () {
         '<div class="panel-group" id="accordion" aria-multiselectable="true"><strong>No Faq Available</strong></div>'
       );
     } else {
-
+      json.forEach((data) => {
+        faqHtml = `<button class="accordion">${data.question}</button><div class="panel" id="panel-${data.id}"><p>${data.answer}</p></div>`;
+        $("#faq").append(faqHtml);
+      });
+      var acc = document.querySelectorAll(".accordion");
+      for (let i = 0; i < acc.length; i++) {
+        var activePanel;
+        acc[i].addEventListener("click", function () {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            if (activePanel) {
+              activePanel.style.maxHeight = null;
+            }
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            activePanel = panel;
+          }
+        });
+      }
     }
-  })
-     
+  });
+  $.ajax({
+    type: "GET",
+    contentType: "application/json",
+    url: site_url + "/policy",
+    success: function (data) {
+      var policyHtml;
+      policyHtml = `<button class="policy-accordion"><h4>CANCELLATION POLICY</h4></button><div class="policy-panel"><p>${data.cancellation}</p></div>`;
+      $("#policy").append(policyHtml);
+      var acc = document.querySelector(".policy-accordion");
+      acc.addEventListener("click", function () {
+        var panel = acc.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    },
+  });
+});
+$.ajax({
+  type: "GET",
+  contentType: "application/json",
+  url: site_url + "/tripNotes",
+  success: function (data) {
+    $("#note").append(data.tripNotes);
+  },
+});
+
+//for fixed nav
+jQuery(function ($) {
+  $(window).scroll(
+    (function fix_element() {
+      var position = $(this).scrollTop();
+
+      $(".section").each(function () {
+        var target = $(this).offset().top;
+        var id = $(this).attr("id");
+        if (position >= target) {
+          $("#navigation > ul > li > a").removeClass("active");
+          $("#navigation > ul > li > a[href=#" + id + "]").addClass("active");
+        }
+      });
+
+      $("#nav").css(
+        $(window).scrollTop() > 400
+          ? {
+              zIndex: 1,
+              position: "fixed",
+              top: "50px",
+            }
+          : { position: "relative", top: "auto" }
+      );
+
+      return fix_element;
+    })()
+  );
 });
